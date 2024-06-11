b0fccb2b72dedd5efad1e17b0f810f5aa52b40b8
Sean Andrie Gadingan <gseanandrie@gmail.com>
2024-06-11 21:51:45 +0400

    Update app.py

--- src/app.py
+++ src/app.py
@@ -91,7 +91,7 @@ def main():
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     """
     st.write(style, unsafe_allow_html=True)
-    st.markdown(f"""by **Sean Gadingan** ({icons['linkedin']}/{icons['kaggle']}/{icons['email']})""", unsafe_allow_html=True)
+    st.markdown(f"""by **Sean Gadingan** ({icons['linkedin']}/{icons['email']})""", unsafe_allow_html=True)
 
     AuthPage = Page()
     MainPage = Page()
